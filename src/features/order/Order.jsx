77ceb1133c837a0efc7e5589066c9bde91d4cc7c
akshayzagad <akshayzagade62@gmail.com.com>
2025-06-22 23:33:17 +0530

    feat: add UpdateOrder component for managing order priority and integrate it into the Order view

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -9,6 +9,7 @@ import {
 } from '../../utils/helpers';
 import OrderItem from './OrderItem';
 import { useEffect } from 'react';
+import UpdateOrder from './UpdateOrder';
 
 function Order() {
   
@@ -95,6 +96,7 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+      {!priority && <UpdateOrder order={order}/>}
     </div>
   );
 }
